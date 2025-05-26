87f469c383c76fc1995bbb9fb5c28ef223f1d59e
Nitesh Jaiswar <niteshjaiswar@Niteshs-MacBook-Air.local>
2025-05-27 04:00:29 +0530

    Adding REST API Grouping all the request

--- index.js
+++ index.js
@@ -2,16 +2,44 @@ const express = require("express"); // importing express
 const users = require("./MOCK_DATA.json");
 const app = express(); // Handler function
 
-app.get("/", (req, res) => {
-  return res.send("hello from home page");
+// HTML response
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+      </ul>`;
+  return res.send(html);
 });
 
-app.get("/users", (req, res) => {
+//REST API
+// Dynamic id
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //Edit user with id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    //Delete
+    return res.json({ status: "pending" });
+  })
+  .post((req, res) => {
+    //POST
+    return res.json({ status: "pending" });
+  });
+
+app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/about", (req, res) => {
-  return res.json("about page you are seeing rn");
+app.get("/users/:first_name", (req, res) => {
+  const first_name = req.params.first_name;
+  const user = users.find((user) => user.first_name === first_name);
+  return res.json(user);
 });
-
 app.listen(8000, () => console.log("server started"));
