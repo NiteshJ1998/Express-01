36551917a1f1e917c9b114698990bfa46cefa4c6
NiteshJ1998 <niteshjaiswar10@gmail.com>
2025-05-29 05:04:39 +0530

    this code is connection to the mongodb

--- index.js
+++ index.js
@@ -14,26 +14,29 @@ mongoose
   .catch((err) => console.log("Connection failed", err));
 
 //Scheama
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  jobTitle: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("user", userSchema);
 
@@ -42,24 +45,24 @@ const User = mongoose.model("user", userSchema);
 // this plugin basically pull the data from postman and created json object and put on request.body
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  console.log("hello fgffrom middleware 1 ");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("hello fgffrom middleware 1 ");
+//   next();
+// });
 
-app.use((req, res, next) => {
-  console.log("hello from middleware 1 ");
-  return res.end("hey fdsfdsadfsfads");
-});
+// app.use((req, res, next) => {
+//   console.log("hello from middleware 1 ");
+//   return res.end("hey fdsfdsadfsfads");
+// });
 //Routes
-app.get("/users", (req, res) => {
-  const html = `
-      <ul>
-      ${users.map((user) => `<li>${user.job_title}</li>`).join("")}
-      </ul>
-      `;
-  res.send(html);
-});
+// app.get("/users", (req, res) => {
+//   const html = `
+//       <ul>
+//       ${users.map((user) => `<li>${user.job_title}</li>`).join("")}
+//       </ul>
+//       `;
+//   res.send(html);
+// });
 
 //REST API
 app
@@ -76,13 +79,35 @@ app
     return res.json({ status: "Pending" });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./Mock_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "Success", id: users.length });
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All field are required..." });
+  }
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
   });
+  return res.status(201).json({ msg: "Success" });
 });
+
+// app.post("/api/users", (req, res) => {
+//   const body = req.body;
+//   users.push({ ...body, id: users.length + 1 });
+//   fs.writeFile("./Mock_DATA.json", JSON.stringify(users), (err, data) => {
+//     return res.json({ status: "Success", id: users.length });
+//   });
+// });
 
 app.get("/api/users/:id", (req, res) => {
   const id = Number(req.params.id);
